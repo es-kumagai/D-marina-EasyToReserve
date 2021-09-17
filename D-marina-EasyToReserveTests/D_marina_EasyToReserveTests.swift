97ff39a39cc43fe68a75526afe64f0b67aabf41e
Tomohiro Kumagai <tomohiro@ez-style.jp>
2021-09-18 00:29:50 +0900

    Initial Commit

---
+++ D-marina-EasyToReserveTests/D_marina_EasyToReserveTests.swift
@@ -0,0 +1,33 @@
+//
+//  D_marina_EasyToReserveTests.swift
+//  D-marina-EasyToReserveTests
+//
+//  Created by Tomohiro Kumagai on 2021/09/18.
+//
+
+import XCTest
+@testable import D_marina_EasyToReserve
+
+class D_marina_EasyToReserveTests: XCTestCase {
+
+    override func setUpWithError() throws {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDownWithError() throws {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() throws {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() throws {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
